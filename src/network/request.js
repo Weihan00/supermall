cfb597fdb13f58f643d68f7bc996b2d785a94bcf
Weihan Pang WX802873 <weihan.pang1@huawei.com>
2021-02-15 19:51:03 +0100

    add recommend and tab,

---
+++ src/network/request.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+
+export function request(config) {
+    const instance = axios.create({
+        // 需要接口的请加coderwhy老师
+        baseURL:'http://123.207.32.32:8000',
+        //baseURL: 'http://adi-v3.dev',
+        //timeout: 1000000
+    })
+    //请求拦截
+    instance.interceptors.request.use(config => {
+        return config
+    }, err => {
+        console.log(err)
+    })
+    //响应拦截
+    instance.interceptors.response.use(result => {
+        return result.data
+    }, err => {
+        console.log(err)
+    })
+    return instance(config)
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,129 @@
 <template>
-    <h2>Home</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center><span>购物商场</span></template>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view />
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '新款', '精选']"
+    ></tab-control>
+    <ul>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+      <li>dd</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import NavBar from "../../components/common/navbar/NavBar.vue";
+import TabControl from "../../components/content/tabControl/TabControl.vue";
+import { getHomeMutilData, getHomeGoodsData } from "../../network/home";
+import FeatureView from "./childComps/FeatureView.vue";
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+
 export default {
-    name: "Home"
-}
+  name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        'pop': {page: 0, list: []},
+        'news': {page:0, list: []},
+        'sell': {page: 0, list: []} 
+      }
+    };
+  },
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl,
+  },
+  created() {
+    getHomeMutilData().then((res) => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+    getHomeGoodsData('pop', 1).then(res=>{
+      console.log(res)
+    })
+  },
+};
 </script>
 
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
 
+.home-nav {
+  background-color: pink;
+  color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+}
 </style>
